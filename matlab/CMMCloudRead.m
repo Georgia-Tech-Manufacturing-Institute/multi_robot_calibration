7bb478a4ee985dcc486ca11072eb30b5115c0d65
Andrew Schneider <aschneiderb4@gmail.com>
2024-09-10 16:10:04 -0400

    edit CMMCloudRead.m

--- matlab/CMMCloudRead.m
+++ matlab/CMMCloudRead.m
@@ -14,27 +14,11 @@
 
 function [W1,W2] = CMMCloudRead(cloud,n)
 %   Automatically imports data from CMM
-indices = find(cloud{:,2}==1);
+[C,ia,idx] = unique(cloud(:,1),'stable');
 
-Wx = zeros(n,1);
-Wy = zeros(n,1);
-Wz = zeros(n,1);
-
-for i=1:1:n-1
-    Wx(i) = mean(cloud{indices(i):1:(indices(i+1)-1),4});
-    Wy(i) = mean(cloud{indices(i):1:(indices(i+1)-1),5});
-    Wz(i) = mean(cloud{indices(i):1:(indices(i+1)-1),6});
-end
-
-if height(indices)==height(cloud)
-    Wx(n) = mean(cloud{indices(n):1:indices(n),4});
-    Wy(n) = mean(cloud{indices(n):1:indices(n),5});
-    Wz(n) = mean(cloud{indices(n):1:indices(n),6});
-else
-    Wx(n) = mean(cloud{indices(n-1):1:indices(n),4});
-    Wy(n) = mean(cloud{indices(n-1):1:indices(n),5});
-    Wz(n) = mean(cloud{indices(n-1):1:indices(n),6});
-end
+Wx = accumarray(idx,cloud{:,4},[],@mean);
+Wy = accumarray(idx,cloud{:,5},[],@mean);
+Wz = accumarray(idx,cloud{:,6},[],@mean);
 
 W1 = [Wx(1:n/2)'; Wy(1:n/2)'; Wz(1:n/2)']/1000;
 W2 = [Wx(n/2+1:n)'; Wy(n/2+1:n)'; Wz(n/2+1:n)']/1000;
