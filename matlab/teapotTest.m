76f26eb094515815e795cc179a157a0e52517eca
Andrew Schneider <aschneiderb4@gmail.com>
2024-09-25 10:23:39 -0400

    edit teapotTest.m

--- matlab/teapotTest.m
+++ matlab/teapotTest.m
@@ -19,8 +19,8 @@ V1_cloud = ptCloud;
 
 eul1_true = [4,37,2];
 trans1_true = [-15,-8,6];
-eul2_true = [43,60,150];
-trans2_true = [13,-10,3];
+eul2_true = [0,0,30];
+trans2_true = [5,5,10];
 
 tform1 = rigidtform3d(eul1_true,trans1_true);
 tform2 = rigidtform3d(eul2_true,trans2_true);
@@ -34,18 +34,18 @@ pcshow(W1_cloud)
 pcshow(W2_cloud)
 
 %% pcregistericp method
-H_W1V1_vision = pcregistericp(V1_cloud,W1_cloud);
-H_W2V1_vision = pcregistericp(V1_cloud,W2_cloud);
+H_W1V1_vision = invert(pcregistericp(W1_cloud,V1_cloud));
+H_W2V1_vision = invert(pcregistericp(W2_cloud,V1_cloud));
 
 %% Plot Stuff!
-H1 = double(H_W1V1_vision.A);
-H2 = double(H_W2V1_vision.A);
+H1_vision = double(H_W1V1_vision.A);
+H2_vision = double(H_W2V1_vision.A);
 
 figure(2)
 hold on
 ax1 = plotTransforms(se3(eye(4,4)),'FrameAxisLabels',"on","FrameLabel","Origin","FrameSize",3);
-ax2 = plotTransforms(se3(H1),'FrameAxisLabels',"on","FrameLabel","W1","FrameSize",3);
-ax3 = plotTransforms(se3(H2),'FrameAxisLabels',"on","FrameLabel","W2","FrameSize",3);
+ax2 = plotTransforms(se3(H1_vision),'FrameAxisLabels',"on","FrameLabel","W1","FrameSize",3);
+ax3 = plotTransforms(se3(H2_vision),'FrameAxisLabels',"on","FrameLabel","W2","FrameSize",3);
 axis padded
 axis equal
 grid on
@@ -84,12 +84,13 @@ eul2_AMPF = flip(double(rad2deg(rotm2eul(H_W2V1_SVD(1:3,1:3)))))
 trans2_AMPF = double(H_W2V1_SVD(1:3,4))
 
 %% Easy Errors
-eul1_vision_error = norm(eul1_true-eul1_vision)
-trans1_vision_error = norm(trans1_true-transpose(trans1_vision))
-eul2_vision_error = norm(eul2_true-eul2_vision)
-trans2_vision_error = norm(trans2_true-transpose(trans2_vision))
+eul1_vision_error = norm(eul1_true-eul1_vision);
+trans1_vision_error = norm(trans1_true-transpose(trans1_vision));
+eul2_vision_error = norm(eul2_true-eul2_vision);
+trans2_vision_error = norm(trans2_true-transpose(trans2_vision));
 
-eul1_AMPF_error = norm(eul1_true-eul1_AMPF)
-trans1_AMPF_error = norm(trans1_true-transpose(trans1_AMPF))
-eul2_AMPF_error = norm(eul2_true-eul2_AMPF)
-trans2_AMPF_error = norm(trans2_true-transpose(trans2_AMPF))
+eul1_AMPF_error = norm(eul1_true-eul1_AMPF);
+trans1_AMPF_error = norm(trans1_true-transpose(trans1_AMPF));
+eul2_AMPF_error = norm(eul2_true-eul2_AMPF);
+trans2_AMPF_error = norm(trans2_true-transpose(trans2_AMPF));
+
