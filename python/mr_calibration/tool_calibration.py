fix rotation matrix creation in wobj calibration

python/mr_calibration/tool_calibration.py
@@ -62,10 +62,13 @@ def main():
     robot = Robot(za6())
     robot.tool = SE3.Rx(np.pi) * SE3.Ry(np.pi / 2)
 
-    initial_guess = np.array([-0.087, 0.00087, 0.012]) * 1000 # mm
-    initial_guess = np.array([0.012, 0.00087, -0.087]) * 1000 # mm
+    initial_guess = np.array([-0.087, 0.00087, 0.012]) * 1000  # mm
+    initial_guess = np.array([0.012, 0.00087, -0.087]) * 1000  # mm
     res = tool_calibration(robot, qq, initial_guess)
-    print(res)
+
+    np.set_printoptions(suppress=True, formatter={"float_kind": "{:0.6f}".format})
+    print(f"Tool translation (mm): {res}")
+    print(f"Tool translation (m): {res / 1000}")
 
 
 if __name__ == "__main__":

python/mr_calibration/wobj_calibration.py
@@ -66,11 +66,12 @@ def main():
     x_axis = x_axis / np.linalg.norm(x_axis)
     y_axis = y_axis / np.linalg.norm(y_axis)
     normal = normal / np.linalg.norm(normal)
-    T_base_wobj = SE3.Rt(np.array([x_axis, y_axis, normal]).T, origin)
+    T_base_wobj = SE3.Rt(np.array([x_axis, y_axis, normal]).T, origin, check=False)
 
     # Transform to world frame
     T_world_wobj = T_world_base * T_base_wobj
 
+    np.set_printoptions(suppress=True, formatter={"float_kind": "{:0.6f}".format})
     print("============ Work Object Transformation ============")
     print(f"T_world_wobj:\n {T_world_wobj}")
 